65c5d57d336e91bd01193af748b3ed11bacd8b91
Valentin Sierra <valentinsierradw@gmail.com>
2026-05-07 19:38:12 -0300

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
     return (
         <footer className="bg-[var(--bg-soft)] text-center py-4 mt-8">
             <p className="text-sm text-[var(--text-outside)]">
-                &copy; {new Date().getFullYear()} Recetas. Todos los derechos reservados.
+                Derechod reservados a valencinn
             </p>
         </footer>
     )
