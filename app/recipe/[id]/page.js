4acbb63c1c5325780757c20988fcfc60e3bc2aa3
Valentin Sierra <valentinsierradw@gmail.com>
2026-05-14 20:32:32 -0300

    ias disclaimer y fix bug comentario sin cerrar

--- app/recipe/[id]/page.js
+++ app/recipe/[id]/page.js
@@ -42,7 +42,7 @@ export default async function RecipeDetailPage({ params }) {
   return (
     <div className="bg-[var(--bg-soft)]">
       <div className="max-w-6xl mx-auto px-4 py-8">
-        {/ */}
+        {/* */}
         <section className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
           {/*fotos */}
           <div className="relative w-full h-[360px]">
